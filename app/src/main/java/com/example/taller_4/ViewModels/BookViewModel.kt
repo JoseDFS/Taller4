84d85ecfe5db2f872bd2527a8ce064d83e59481c
EdwinLovo <00120617@uca.edu.sv>
2019-05-25 21:01:06 -0600

    Agregando ViewModel

--- app/src/main/java/com/example/taller_4/ViewModels/BookViewModel.kt
+++ app/src/main/java/com/example/taller_4/ViewModels/BookViewModel.kt
@@ -4,42 +4,30 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.viewModelScope
+import com.example.taller_4.Database.myRoomDatabase
 import com.example.taller_4.Repository.AutorRepository
 import com.example.taller_4.Repository.FavoritoRepository
 import com.example.taller_4.Repository.LibroRepository
+import com.example.taller_4.entities.Book
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class BookViewModel(application: Application): AndroidViewModel(application) {
 
-    private val libroRepository: LibroRepository
-    private val favoritoRepository: FavoritoRepository
-    private val autorRepository: AutorRepository
-    val allLibros: LiveData<List<Libro>>
-    val allFavoritos: LiveData<List<Favorito>>
-    val allAutor: LiveData<Autor>
+    private val repository: LibroRepository
+
+    val allBooks: LiveData<List<Book>>
+    val allFavorite: LiveData<List<Book>>
 
     init {
-        val librosDao = RoomDB.getInstance(application).libroDao()
-        val favoritosDao = RoomDB.getInstance(application).favoritosDao()
-        val autoresDao = RoomDB.getInstance(application).autorDao()
-        libroRepository = LibroRepository(librosDao)
-        favoritoRepository = FavoritoRepository(favoritosDao)
-        autorRepository = AutorRepository(autoresDao)
-        allLibros = libroRepository.allBooks
-        allFavoritos = favoritoRepository.allFavoritos
-        allAutor = autorRepository.autor
+        val booksDao = myRoomDatabase.getDatabase(application, viewModelScope).bookDao()
+        repository = LibroRepository(booksDao)
+        allBooks = repository.allBooks
+        allFavorite = repository.getFavorites
     }
 
-    fun insertLibro(libro: Libro) = viewModelScope.launch(Dispatchers.IO){
-        libroRepository.insert(libro)
+    fun insert(book: Book) = viewModelScope.launch{
+        repository.insert(book)
     }
 
-    fun insertAutor(autor: Autor) = viewModelScope.launch(Dispatchers.IO){
-        autorRepository.insertAutor(autor)
-    }
-
-    fun insertFavorito(favorito: Libro) = viewModelScope.launch(Dispatchers.IO){
-        favoritoRepository.insertFavorite(favorito)
-    }
 }
